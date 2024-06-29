dd03ac6b5d5b49ac25f0bcf5a8bbc83ba4507c38
hazem_smawy <hazemsmawy@gmail.com>
2024-06-29 15:32:39 +0300

    Initial Commit

---
+++ era_pro/era_proApp.swift
@@ -0,0 +1,17 @@
+//
+//  era_proApp.swift
+//  era_pro
+//
+//  Created by B-ALDUAIS on 29/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct era_proApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
